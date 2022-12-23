74b81e779160ee8cfe10736cd37857c63fa1d5bf
Kamil Gębala <kamazowe@gmail.com>
2022-12-23 23:13:01 +0100

    initial commit

---
+++ apps/customer/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ddd-architecture-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'customer';
+}
