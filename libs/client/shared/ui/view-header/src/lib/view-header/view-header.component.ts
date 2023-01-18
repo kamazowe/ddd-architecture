531b00241585115d38f1b58c8ab086e673bb779c
Stasiek99 <103383789+Stasiek99@users.noreply.github.com>
2023-01-18 20:34:38 +0100

    feat(shared): added view-header

--- libs/client/shared/ui/view-header/src/lib/view-header/view-header.component.ts
+++ libs/client/shared/ui/view-header/src/lib/view-header/view-header.component.ts
@@ -7,16 +7,20 @@ import {
 } from '@angular/core';
 import { JsonPipe } from '@angular/common';
 import { UiViewHeader } from './view-header.interface';
-
+import { TuiButtonModule, TuiSvgModule } from '@taiga-ui/core';
 @Component({
   selector: 'ddd-view-header',
   standalone: true,
-  imports: [JsonPipe],
+  imports: [JsonPipe, TuiButtonModule, TuiSvgModule],
   templateUrl: './view-header.component.html',
   styleUrls: ['./view-header.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ViewHeaderComponent {
   @Input() header: UiViewHeader | null = null;
-  @Output() backButtonClicked = new EventEmitter<void>();
+  @Output() prevButtonClicked = new EventEmitter<void>();
+
+  onClickPrevButton(): void {
+    this.prevButtonClicked.emit();
+  }
 }
