b44c02ea2c343a2b204d316af17d2b3827e65ca3
Stasiek99 <103383789+Stasiek99@users.noreply.github.com>
2023-01-23 16:12:06 +0100

    feat(shared): part card layout

--- libs/client/use-cases/part/list/shared/ui/src/lib/part-card/part-card.component.ts
+++ libs/client/use-cases/part/list/shared/ui/src/lib/part-card/part-card.component.ts
@@ -2,6 +2,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TuiButtonModule, TuiSvgModule } from '@taiga-ui/core';
 import { TuiIslandModule } from '@taiga-ui/kit';
+import { UiPartCard } from './part-card.interface';
 
 @Component({
   selector: 'ddd-part-card',
@@ -11,4 +12,6 @@ import { TuiIslandModule } from '@taiga-ui/kit';
   styleUrls: ['./part-card.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PartCardComponent {}
+export class PartCardComponent {
+  partCard: UiPartCard | null = null;
+}
