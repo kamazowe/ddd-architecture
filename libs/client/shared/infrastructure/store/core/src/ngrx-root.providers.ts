495dc456f242a18ec52c35b9af35b1653cfaf47d
Kamil Gębala <kamazowe@gmail.com>
2023-02-10 23:23:33 +0100

    feat(router): added ngrx router reducer

--- libs/client/shared/infrastructure/store/core/src/ngrx-root.providers.ts
+++ libs/client/shared/infrastructure/store/core/src/ngrx-root.providers.ts
@@ -1,11 +1,24 @@
 import { EnvironmentProviders } from '@angular/core';
-import { provideStore } from '@ngrx/store';
+import { ActionReducerMap, provideStore } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
-import { provideRouterStore } from '@ngrx/router-store';
+import {
+  provideRouterStore,
+  routerReducer,
+  RouterReducerState,
+} from '@ngrx/router-store';
+import { SerializedRouterStateSnapshot } from '@ngrx/router-store/src/serializers/full_serializer';
+
+export interface RouterState {
+  router: RouterReducerState<SerializedRouterStateSnapshot>;
+}
+
+export const reducers: ActionReducerMap<RouterState> = {
+  router: routerReducer,
+};
 
 export const ngrxRootProviders: EnvironmentProviders[] = [
-  provideStore(),
+  provideStore(reducers),
   provideEffects([]),
   provideStoreDevtools(),
   provideRouterStore(),
